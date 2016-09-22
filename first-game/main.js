e78a6441f0f183eecf88b7a21be4952f2791ef7a
Guillaume Benoit <guillaume.benoit@blablacar.com>
2016-09-22 18:05:15 +0200

    Empty game

---
+++ first-game/main.js
@@ -0,0 +1,17 @@
+var mainState = {
+    preload: function() {
+        
+    },
+    create: function() {
+        game.stage.backgroundColor = '#3498db';
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.renderer.renderSession.roundPixels = true;
+    },
+    update: function() {
+        
+    }
+};
+
+var game = new Phaser.Game(500, 340, Phaser.AUTO, 'gameDiv');
+game.state.add('main', mainState);
+game.state.start('main');
